61a3278eeaf3114dd3df944110c45ffccb0bfcb5
Syny <s.raouf@student.isartdigital.com>
2018-04-26 12:05:53 -0400

    Character Walk + Run

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CapsuleCollider))]
+public class Movement : MonoBehaviour
+{
+    private Animator anim;
+    private bool isWalking = false;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        //Turning
+        //Jumping
+        //Move
+        Walk();
+        Move();
+    }
+
+    void Walk()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            isWalking = !isWalking;
+            anim.SetBool("Walk", isWalking);
+        }
+    }
+
+    void Move()
+    {
+        anim.SetFloat("Forward", Input.GetAxis("Vertical"));
+    }
+
+}
